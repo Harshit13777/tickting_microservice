cab59a901eb75f379e186ad8b1d665577d984cea
Harshit <soniharshit186@gmail.com>
2024-08-06 12:33:42 +0530

    signin completed

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
+import jwt from "jsonwebtoken";
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send('hello')
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null })
+    };
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+        res.send({ currentUser: payload })
+    } catch (e: any) {
+        res.send({ currentUser: null })
+    }
+
 })
 
 export { router as currentUserRouter };
