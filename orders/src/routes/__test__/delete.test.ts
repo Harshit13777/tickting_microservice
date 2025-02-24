7e95ae06504ea8abd6d0fc2fa7305240ef8b3f52
Harshit <soniharshit186@gmail.com>
2025-02-25 05:24:23 +0530

    orders service update version

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,10 +2,12 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/tickets';
 import { Order, OrderStatus } from '../../models/orders';
+import mongoose from 'mongoose';
 
 it('marks an order as cancelled', async () => {
   // create a ticket with Ticket Model
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   });
